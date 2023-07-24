c18fc7cd89004e110f4ad26f116f2e078811c951
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-07-24 17:06:50 +0100

    update example input references

--- cishouseholds/pipeline/input_file_stages.py
+++ cishouseholds/pipeline/input_file_stages.py
@@ -1,81 +1,77 @@
 from cishouseholds.pipeline.mapping import column_name_maps
-from cishouseholds.pipeline.mapping import test_participant_data_cast_to_double
-from cishouseholds.pipeline.mapping import test_survey_response_data_cast_to_double
+from cishouseholds.pipeline.mapping import example_participant_data_cast_to_double
+from cishouseholds.pipeline.mapping import example_survey_response_data_cast_to_double
 from cishouseholds.pipeline.pipeline_stages import generate_input_processing_function
-from cishouseholds.pipeline.timestamp_map import test_participant_data_datetime_map
-from cishouseholds.pipeline.timestamp_map import test_survey_response_data_version_1_datetime_map
-from cishouseholds.pipeline.timestamp_map import test_survey_response_data_version_2_datetime_map
-from cishouseholds.pipeline.timestamp_map import test_swab_sample_results_datetime_map
+from cishouseholds.pipeline.timestamp_map import example_participant_data_datetime_map
+from cishouseholds.pipeline.timestamp_map import example_survey_response_data_v1_datetime_map
+from cishouseholds.pipeline.timestamp_map import example_survey_response_data_v2_datetime_map
+from cishouseholds.pipeline.timestamp_map import example_swab_sample_results_datetime_map
 from cishouseholds.pipeline.validation_schema import validation_schemas
 from cishouseholds.pipeline.version_specific_processing.example_participant_data import (
-    transform_participant_extract_digital,
+    transform_example_participant_extract,
 )
 from cishouseholds.pipeline.version_specific_processing.example_survey_response_data_v1 import (
     example_survey_response_data_v1_transformations,
 )
 from cishouseholds.pipeline.version_specific_processing.example_survey_response_data_v2 import (
-    clean_survey_responses_version_2,
-)
-from cishouseholds.pipeline.version_specific_processing.example_survey_response_data_v2 import (
-    transform_survey_responses_version_2_delta,
+    example_survey_response_data_v2_transformations,
 )
 
 
-test_participant_data_parameters = {
+example_participant_data_parameters = {
     "stage_name": "test_participant_data_ETL",
     "dataset_name": "test_participant_data",
     "id_column": "participant_id",
-    "validation_schema": validation_schemas["test_participant_data_schema"],
-    "column_name_map": column_name_maps["test_participant_data_name_map"],
-    "datetime_column_map": test_participant_data_datetime_map,
+    "validation_schema": validation_schemas["example_participant_data_schema"],
+    "column_name_map": column_name_maps["example_participant_data_name_map"],
+    "datetime_column_map": example_participant_data_datetime_map,
     "transformation_functions": [
-        transform_participant_extract_digital,
+        transform_example_participant_extract,
     ],
     "sep": "|",
-    "cast_to_double_list": test_participant_data_cast_to_double,
+    "cast_to_double_list": example_participant_data_cast_to_double,
     "source_file_column": "participant_data_source_file",
 }
 
-test_survey_response_data_version_1_parameters = {
+example_survey_response_data_v1_parameters = {
     "stage_name": "test_survey_response_data_version_1_ETL",
     "dataset_name": "test_survey_response_data_version_1",
     "id_column": "participant_completion_window_id",
-    "validation_schema": validation_schemas["test_survey_response_data_version_1_schema"],
-    "column_name_map": column_name_maps["test_survey_response_data_version_1_name_map"],
-    "datetime_column_map": test_survey_response_data_version_1_datetime_map,
+    "validation_schema": validation_schemas["example_survey_response_data_v1_schema"],
+    "column_name_map": column_name_maps["example_survey_response_data_v1_name_map"],
+    "datetime_column_map": example_survey_response_data_v1_datetime_map,
     "transformation_functions": [
         example_survey_response_data_v1_transformations,
     ],
     "sep": "|",
-    "cast_to_double_list": test_survey_response_data_cast_to_double,
+    "cast_to_double_list": example_survey_response_data_cast_to_double,
     "source_file_column": "survey_response_source_file",
     "survey_table": True,
 }
 
-test_survey_response_data_version_2_parameters = {
+example_survey_response_data_v2_parameters = {
     "stage_name": "test_survey_response_data_version_2_ETL",
     "dataset_name": "test_survey_response_data_version_2",
     "id_column": "participant_completion_window_id",
-    "validation_schema": validation_schemas["test_survey_response_data_version_2_schema"],
-    "column_name_map": column_name_maps["test_survey_response_data_version_2_name_map"],
-    "datetime_column_map": test_survey_response_data_version_2_datetime_map,
+    "validation_schema": validation_schemas["example_survey_response_data_v2_schema"],
+    "column_name_map": column_name_maps["example_survey_response_data_v2_name_map"],
+    "datetime_column_map": example_survey_response_data_v2_datetime_map,
     "transformation_functions": [
-        clean_survey_responses_version_2,
-        transform_survey_responses_version_2_delta,
+        example_survey_response_data_v2_transformations,
     ],
     "sep": "|",
-    "cast_to_double_list": test_survey_response_data_cast_to_double,
+    "cast_to_double_list": example_survey_response_data_cast_to_double,
     "source_file_column": "survey_response_source_file",
     "survey_table": True,
 }
 
-test_swab_sample_results_parameters = {
+example_swab_sample_results_parameters = {
     "stage_name": "test_swab_sample_results_ETL",
     "dataset_name": "test_swab_sample_results",
     "id_column": "Sample",
-    "validation_schema": validation_schemas["test_swab_sample_results_schema"],
-    "datetime_column_map": test_swab_sample_results_datetime_map,
-    "column_name_map": column_name_maps["test_swab_sample_results_name_map"],
+    "validation_schema": validation_schemas["example_swab_sample_results_schema"],
+    "column_name_map": column_name_maps["example_swab_sample_results_name_map"],
+    "datetime_column_map": example_swab_sample_results_datetime_map,
     "transformation_functions": [],
     "sep": ",",
     "cast_to_double_list": [],
@@ -84,9 +80,9 @@ test_swab_sample_results_parameters = {
 }
 
 for parameters in [
-    test_participant_data_parameters,
-    test_survey_response_data_version_1_parameters,
-    test_survey_response_data_version_2_parameters,
-    test_swab_sample_results_parameters,
+    example_participant_data_parameters,
+    example_survey_response_data_v1_parameters,
+    example_survey_response_data_v2_parameters,
+    example_swab_sample_results_parameters,
 ]:
     generate_input_processing_function(**parameters)  # type:ignore
--- cishouseholds/pipeline/timestamp_map.py
+++ cishouseholds/pipeline/timestamp_map.py
@@ -8,9 +8,9 @@ csv_datetime_maps = {
     },
 }
 
-test_swab_sample_results_datetime_map = {"yyyy-MM-dd HH:mm:ss 'UTC'": ["pcr_result_recorded_datetime"]}
+example_swab_sample_results_datetime_map = {"yyyy-MM-dd HH:mm:ss 'UTC'": ["pcr_result_recorded_datetime"]}
 
-test_participant_data_datetime_map = {
+example_participant_data_datetime_map = {
     "yyyy-MM-dd'T'HH:mm:ss'Z'": [
         "participant_completion_window_start_datetime",
         "participant_completion_window_end_datetime",
@@ -82,7 +82,7 @@ test_participant_data_datetime_map = {
     ],
 }
 
-test_survey_response_data_version_1_datetime_map = {
+example_survey_response_data_v1_datetime_map = {
     "yyyy-MM-dd": [
         "swab_taken_date",
         "swab_return_date",
@@ -106,7 +106,7 @@ test_survey_response_data_version_1_datetime_map = {
     "yyyy-MM-dd'T'HH:mm:ss": ["survey_start_datetime"],
 }
 
-test_survey_response_data_version_2_datetime_map = {
+example_survey_response_data_v2_datetime_map = {
     "yyyy-MM-dd": [
         "swab_taken_date",
         "swab_return_date",
